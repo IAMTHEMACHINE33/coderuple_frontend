49f783d570660ffdb57af4fbc5bc6a6f26aa10f3
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-03-26 16:29:44 +0545

    added new font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import '../styles/navbar.css'
 import '../styles/testimonial.scss'
+import { Open_Sans } from '@next/font/google'
+
+const open_sans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={open_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
