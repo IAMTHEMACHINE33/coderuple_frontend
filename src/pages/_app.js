9130184de7217016cbc5f084ab0fe9bc5968ab49
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-03-27 18:11:41 +0545

    tailwind elements setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/navbar.css'
 import '../styles/testimonial.scss'
 import { Open_Sans } from '@next/font/google'
+import { useEffect } from 'react'
 
 const open_sans = Open_Sans({
   subsets: ['latin'],
@@ -9,9 +10,18 @@ const open_sans = Open_Sans({
 })
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    const use = async () => {
+      (await import('tw-elements')).default;
+    };
+    use();
+  }, []);
+
   return (
     <main className={open_sans.className}>
       <Component {...pageProps} />
     </main>
   )
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
+    "./node_modules/tw-elements/dist/js/**/*.js",
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")]
 }
