f8123b339463909445b00f6c546f52811d6bd8ad
Drunken72 <arushasp4710@gmail.com>
2023-03-30 13:40:33 +0545

    contact us with 1 missing info

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,34 +2,21 @@ import Image from "next/image";
 import HomePageHeader from "../UI/HomePageHeader";
 import blue_wave from "./blue_wave_up_down.png";
 import ContactUsForm from "./ContactUsForm";
+import ContactUsInfo from "./ContactUsInfo";
 
 const ContactUs = () => {
-    return (
-        <div className="relative">
-            <div className="flex flex-col items-center justify-center relative z-50">
-                <HomePageHeader text="Concact Us" />
-                <div className="flex justify-center items-center">
-                    <div className="w-1/2">
-                        <div>
-                            <p>Let’s Work Together!</p>
-                            <p>Please tell us a bit about you, your project, and how best to reach you. We’ll get right back to you.</p>
-                        </div>
-                        <div>
-                            <p>Customer Service representative:</p>
-                            <p>+977 9813643643</p>
-                            <p>+977 9813643643</p>
-                        </div>
-                        <div>
-                            <p>Do you prefer email?</p>
-                            <p>coderupleglobal@gmail.com</p>
-                        </div>
-                    </div>
-                    <ContactUsForm />
-                </div>
-            </div>
-            <Image src={blue_wave} className="absolute top-0 z-10 w-full h-[53rem]" />
+  return (
+    <div className="relative">
+      <div className="flex flex-col items-center justify-center mt-20 relative z-50">
+        <HomePageHeader className="mb-10" text="Contact Us" />
+        <div className="flex flex-col mt-20 items-center xs:justify-end md:flex-row md:max-w md:mx-10 ">
+          <ContactUsInfo />
+          <ContactUsForm />
         </div>
-    )
-}
+      </div>
+      <Image src={blue_wave} className="absolute top-0 z-10 w-full h-[53rem]" />
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
