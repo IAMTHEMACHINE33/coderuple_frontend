f81cf441302680234d48e998528f0618f86d79a4
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-04-05 20:25:39 +0545

    used mui table for testing

--- src/pages/admin/testimonials.jsx
+++ src/pages/admin/testimonials.jsx
@@ -1,9 +1,14 @@
+import EnhancedTable from "@/components/UI/EnhancedTable"
+import QuickFilteringInitialize from "@/components/UI/QuickFilteringInitialize"
+
 const { default: AdminSideNavContainer } = require("@/components/Admin/AdminNavbar/AdminContainer")
 
 const testimonials = () => {
   return (
     <AdminSideNavContainer>
       <div>testimonials</div>
+      {/* <EnhancedTable /> */}
+      <QuickFilteringInitialize />
     </AdminSideNavContainer>
   )
 }
