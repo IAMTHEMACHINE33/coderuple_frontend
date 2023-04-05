fa6759513c93c1f322725065e5b0b46bd31f9a41
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-04-05 18:10:26 +0545

    added scroll to nav links

--- src/components/Admin/AdminNavbar/AdminSideNav.jsx
+++ src/components/Admin/AdminNavbar/AdminSideNav.jsx
@@ -4,7 +4,7 @@ const AdminSideNav = () => {
     return (
         <nav
             id="sidenav-1"
-            class="fixed top-0 left-0 z-[1035] h-screen w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] dark:bg-zinc-800 xl:data-[te-sidenav-hidden='false']:translate-x-0"
+            class="fixed top-0 left-0 z-[1035] h-screen w-60 -translate-x-full overflow-y-auto bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] dark:bg-zinc-800 xl:data-[te-sidenav-hidden='false']:translate-x-0"
             data-te-sidenav-init
             data-te-sidenav-hidden="false"
             data-te-sidenav-mode-breakpoint-over="0"
