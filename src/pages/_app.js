291b7d66f4ac9594fa70c5b001ef53df2fa035b1
Drunken72 <arushasp4710@gmail.com>
2023-04-16 15:56:21 +0545

    font poppins added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,28 @@
-import '../styles/globals.css'
-import '../styles/navbar.css'
-import '../styles/testimonial.scss'
-import '../styles/homepage.css'
-import { Open_Sans } from '@next/font/google'
-import { useEffect } from 'react'
+import "../styles/globals.css";
+import "../styles/navbar.css";
+import "../styles/testimonial.scss";
+import "../styles/homepage.css";
+// import { Open_Sans } from '@next/font/google'
+import { useEffect } from "react";
 
-const open_sans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700']
-})
+// const open_sans = Open_Sans({
+//   subsets: ['latin'],
+//   weight: ['400', '700']
+// })
 
 export default function App({ Component, pageProps }) {
-
   useEffect(() => {
     const use = async () => {
-      (await import('tw-elements')).default;
+      (await import("tw-elements")).default;
     };
     use();
   });
 
   return (
-    <main className={open_sans.className}>
+    <main
+    // className={open_sans.className}
+    >
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
-
