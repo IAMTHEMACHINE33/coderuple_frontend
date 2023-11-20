5be3e29e82d4503bac2a79b6f29e56a84b10b469
Sanij <Sanizsanij@gmail.com>
2023-11-20 13:10:45 +0545

    feat[home]:testing facebook chatbot

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ import { SessionProvider } from "next-auth/react";
 //   subsets: ['latin'],
 //   weight: ['400', '700']
 // })
-
+import Script from 'next/script'
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
@@ -26,12 +26,39 @@ export default function App({
 
   return (
     <SessionProvider session={session}>
-    <Head>
-      <link rel="shortcut icon" href={favicon.src} />
-    </Head>
+      <Head>
+        <link rel="shortcut icon" href={favicon.src} />
+
+        {/* Facebook Customer Chat Script */}
+        <Script>
+          {`
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml: true,
+        version: 'v18.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "166029633263483");
+    chatbox.setAttribute("attribution", "biz_inbox");
+  `}
+        </Script>
+      </Head>
       <main
       // className={open_sans.className}
       >
+        <div id="fb-root"></div>
+
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
         <Component {...pageProps} />
       </main>
     </SessionProvider>
